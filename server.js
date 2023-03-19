0c049202c50f24d5ae52be4fc45375464d38f51d
NHNerd <promenader.off@gmail.com>
2023-03-19 16:18:45 +0400

    Add express js server(local), the site is now working on the server. in connection with this, the file structure of the project has been changed.

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs'); // Connecting modul for application
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('homepage'); // render - function from ejs mudul
+});
+
+app.get('/razval', (req, res) => {
+  res.render('razval'); // render - function from ejs mudul
+});
+
+app.get('/dubrovsky', (req, res) => {
+  res.render('dubrovsky'); // render - function from ejs mudul
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started.\nhttp://localhost:${PORT}`);
+});
